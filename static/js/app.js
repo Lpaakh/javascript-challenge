40103f2c824cbbbef428d69fa78ce6d973967360
Lpaakh <laurapaakh@gmail.com>
2020-09-11 20:51:02 -0700

    adding starter code

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var columns = {
+    'date/time': date, 
+    'city': cities, 
+    'state': states, 
+    'country': countries, 
+    'shape': shapes, 
+    'comment': comments
+}
